98bb09883886dd23a40d732532866ca7d248379d
Nakai <nakai.hamlin18@gmail.com>
2021-05-04 01:09:14 -0400

    commit tree d1598ddebb92b83748ae362f1fdf14b52a29807b

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,14 +3,28 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+
   def index
     @recipes = Recipe.all
   end
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+      @recipe.ingredients.build
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [
+      :name,
+      :quantity
+      ])
   end
 end
